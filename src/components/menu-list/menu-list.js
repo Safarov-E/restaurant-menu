3632223ff33d15096b3df560eaea57d4b2b05e62
Safarov <elkhan.safarov10@yandex.com>
2020-07-04 21:55:36 +0500

    Оптимизация кода

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
 import {connect} from 'react-redux';
 import WithRestoService from '../hoc';
+import {menuLoaded} from '../../actions';
 
 import './menu-list.scss';
 
@@ -33,15 +34,8 @@ const mapStateToProps = (state) => {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        menuLoaded: (newMenu) => {
-            dispatch({
-                type: 'MENU_LOADED',
-                payload: newMenu
-            })
-        }
-    }
+const mapDispatchToProps = {
+    menuLoaded
 }
 
 
