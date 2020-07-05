93f01aebbaf58adce3daa06be2ed27961f04d0f8
Safarov <elkhan.safarov10@yandex.com>
2020-07-05 12:07:21 +0500

    Вывод корзины товаров

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,22 @@
 const initialState = {
-    menu: []
+    menu: [],
+    loading: true,
+    items: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'MENU_LOADED':
             return {
-                menu: action.payload
+                ...state,
+                menu: action.payload,
+                loading: false
+            }
+        case 'MENU_REQUESTED':
+            return {
+                ...state,
+                menu: state.menu,
+                loading: true
             }
         default:
             return state
